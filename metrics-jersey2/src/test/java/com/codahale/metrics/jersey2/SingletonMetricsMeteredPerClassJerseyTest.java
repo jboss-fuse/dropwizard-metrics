dropwizard/metrics

Register metrics defined at Resource level
If a `Resource` class is annotated with either:

* `@Timed`,
* `@Metered`
* `@ExceptionMetered`

all method defined in such class will 'inherit'
the corresponding annotation.

metrics-jersey2/src/test/java/io/dropwizard/metrics/jersey2/SingletonMetricsMeteredPerClassJerseyTest.java
@@ -1,0 +1,67 @@
+package io.dropwizard.metrics.jersey2;
+
+import io.dropwizard.metrics.Meter;
+import io.dropwizard.metrics.MetricRegistry;
+import io.dropwizard.metrics.jersey2.resources.InstrumentedResourceMeteredPerClass;
+import io.dropwizard.metrics.jersey2.resources.InstrumentedSubResourceMeteredPerClass;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import javax.ws.rs.core.Application;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static io.dropwizard.metrics.MetricRegistry.name;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests registering {@link InstrumentedResourceMethodApplicationListener} as a singleton
+ * in a Jersey {@link ResourceConfig}
+ */
+
+public class SingletonMetricsMeteredPerClassJerseyTest extends JerseyTest {
+    static {
+        Logger.getLogger("org.glassfish.jersey").setLevel(Level.OFF);
+    }
+
+    private MetricRegistry registry;
+
+    @Override
+    protected Application configure() {
+        this.registry = new MetricRegistry();
+
+        ResourceConfig config = new ResourceConfig();
+
+        config = config.register(new MetricsFeature(this.registry));
+        config = config.register(InstrumentedResourceMeteredPerClass.class);
+
+        return config;
+    }
+
+    @Test
+    public void meteredPerClassMethodsAreMetered() {
+        assertThat(target("meteredPerClass")
+                .request()
+                .get(String.class))
+                .isEqualTo("yay");
+
+        final Meter meter = registry.meter(name(InstrumentedResourceMeteredPerClass.class, "meteredPerClass"));
+
+        assertThat(meter.getCount()).isEqualTo(1);
+    }
+
+    @Test
+    public void subresourcesFromLocatorsRegisterMetrics() {
+        assertThat(target("subresource/meteredPerClass")
+                .request()
+                .get(String.class))
+                .isEqualTo("yay");
+
+        final Meter meter = registry.meter(name(InstrumentedSubResourceMeteredPerClass.class, "meteredPerClass"));
+        assertThat(meter.getCount()).isEqualTo(1);
+
+    }
+
+
+}

metrics-jersey2/src/test/java/io/dropwizard/metrics/jersey2/SingletonMetricsTimedPerClassJerseyTest.java
@@ -1,0 +1,67 @@
+package io.dropwizard.metrics.jersey2;
+
+import io.dropwizard.metrics.MetricRegistry;
+import io.dropwizard.metrics.Timer;
+import io.dropwizard.metrics.jersey2.resources.InstrumentedResourceTimedPerClass;
+import io.dropwizard.metrics.jersey2.resources.InstrumentedSubResourceTimedPerClass;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import javax.ws.rs.core.Application;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static io.dropwizard.metrics.MetricRegistry.name;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests registering {@link InstrumentedResourceMethodApplicationListener} as a singleton
+ * in a Jersey {@link ResourceConfig}
+ */
+
+public class SingletonMetricsTimedPerClassJerseyTest extends JerseyTest {
+    static {
+        Logger.getLogger("org.glassfish.jersey").setLevel(Level.OFF);
+    }
+
+    private MetricRegistry registry;
+
+    @Override
+    protected Application configure() {
+        this.registry = new MetricRegistry();
+
+        ResourceConfig config = new ResourceConfig();
+
+        config = config.register(new MetricsFeature(this.registry));
+        config = config.register(InstrumentedResourceTimedPerClass.class);
+
+        return config;
+    }
+
+    @Test
+    public void timedPerClassMethodsAreTimed() {
+        assertThat(target("timedPerClass")
+                .request()
+                .get(String.class))
+                .isEqualTo("yay");
+
+        final Timer timer = registry.timer(name(InstrumentedResourceTimedPerClass.class, "timedPerClass"));
+
+        assertThat(timer.getCount()).isEqualTo(1);
+    }
+
+    @Test
+    public void subresourcesFromLocatorsRegisterMetrics() {
+        assertThat(target("subresource/timedPerClass")
+                .request()
+                .get(String.class))
+                .isEqualTo("yay");
+
+        final Timer timer = registry.timer(name(InstrumentedSubResourceTimedPerClass.class, "timedPerClass"));
+        assertThat(timer.getCount()).isEqualTo(1);
+
+    }
+
+
+}

metrics-jersey2/src/test/java/io/dropwizard/metrics/jersey2/resources/InstrumentedResourceExceptionMeteredPerClass.java
@@ -1,0 +1,28 @@
+package io.dropwizard.metrics.jersey2.resources;
+
+import io.dropwizard.metrics.annotation.ExceptionMetered;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+
+@ExceptionMetered(cause = IOException.class)
+@Path("/")
+@Produces(MediaType.TEXT_PLAIN)
+public class InstrumentedResourceExceptionMeteredPerClass {
+
+    @GET
+    @Path("/exception-metered")
+    public String exceptionMetered(@QueryParam("splode") @DefaultValue("false") boolean splode) throws IOException {
+        if (splode) {
+            throw new IOException("AUGH");
+        }
+        return "fuh";
+    }
+
+    @Path("/subresource")
+    public InstrumentedSubResourceExceptionMeteredPerClass locateSubResource() {
+        return new InstrumentedSubResourceExceptionMeteredPerClass();
+    }
+
+}

metrics-jersey2/src/test/java/io/dropwizard/metrics/jersey2/resources/InstrumentedResourceMeteredPerClass.java
@@ -1,0 +1,26 @@
+package io.dropwizard.metrics.jersey2.resources;
+
+import io.dropwizard.metrics.annotation.Metered;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Metered
+@Path("/")
+@Produces(MediaType.TEXT_PLAIN)
+public class InstrumentedResourceMeteredPerClass {
+
+    @GET
+    @Path("/meteredPerClass")
+    public String meteredPerClass() {
+        return "yay";
+    }
+
+    @Path("/subresource")
+    public InstrumentedSubResourceMeteredPerClass locateSubResource() {
+        return new InstrumentedSubResourceMeteredPerClass();
+    }
+
+}

metrics-jersey2/src/test/java/io/dropwizard/metrics/jersey2/resources/InstrumentedResourceTimedPerClass.java
@@ -1,0 +1,26 @@
+package io.dropwizard.metrics.jersey2.resources;
+
+import io.dropwizard.metrics.annotation.Timed;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Timed
+@Path("/")
+@Produces(MediaType.TEXT_PLAIN)
+public class InstrumentedResourceTimedPerClass {
+
+    @GET
+    @Path("/timedPerClass")
+    public String timedPerClass() {
+        return "yay";
+    }
+
+    @Path("/subresource")
+    public InstrumentedSubResourceTimedPerClass locateSubResource() {
+        return new InstrumentedSubResourceTimedPerClass();
+    }
+
+}

metrics-jersey2/src/test/java/io/dropwizard/metrics/jersey2/resources/InstrumentedSubResourceExceptionMeteredPerClass.java
@@ -1,0 +1,20 @@
+package io.dropwizard.metrics.jersey2.resources;
+
+import io.dropwizard.metrics.annotation.ExceptionMetered;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+
+@ExceptionMetered(cause = IOException.class)
+@Produces(MediaType.TEXT_PLAIN)
+public class InstrumentedSubResourceExceptionMeteredPerClass {
+    @GET
+    @Path("/exception-metered")
+    public String exceptionMetered(@QueryParam("splode") @DefaultValue("false") boolean splode) throws IOException {
+        if (splode) {
+            throw new IOException("AUGH");
+        }
+        return "fuh";
+    }
+}

metrics-jersey2/src/test/java/io/dropwizard/metrics/jersey2/resources/InstrumentedSubResourceMeteredPerClass.java
@@ -1,0 +1,18 @@
+package io.dropwizard.metrics.jersey2.resources;
+
+import io.dropwizard.metrics.annotation.Metered;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Metered
+@Produces(MediaType.TEXT_PLAIN)
+public class InstrumentedSubResourceMeteredPerClass {
+    @GET
+    @Path("/meteredPerClass")
+    public String meteredPerClass() {
+        return "yay";
+    }
+}

jboss-fuse/dropwizard-metrics

metrics-jersey2/src/test/java/com/codahale/metrics/jersey2/SingletonMetricsMeteredPerClassJerseyTest.java
@@ -1,0 +1,66 @@
+package com.codahale.metrics.jersey2;
+
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.jersey2.resources.InstrumentedResourceMeteredPerClass;
+import com.codahale.metrics.jersey2.resources.InstrumentedSubResourceMeteredPerClass;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import javax.ws.rs.core.Application;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static com.codahale.metrics.MetricRegistry.name;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests registering {@link InstrumentedResourceMethodApplicationListener} as a singleton
+ * in a Jersey {@link ResourceConfig}
+ */
+public class SingletonMetricsMeteredPerClassJerseyTest extends JerseyTest {
+    static {
+        Logger.getLogger("org.glassfish.jersey").setLevel(Level.OFF);
+    }
+
+    private MetricRegistry registry;
+
+    @Override
+    protected Application configure() {
+        this.registry = new MetricRegistry();
+
+        ResourceConfig config = new ResourceConfig();
+
+        config = config.register(new MetricsFeature(this.registry));
+        config = config.register(InstrumentedResourceMeteredPerClass.class);
+
+        return config;
+    }
+
+    @Test
+    public void meteredPerClassMethodsAreMetered() {
+        assertThat(target("meteredPerClass")
+                .request()
+                .get(String.class))
+                .isEqualTo("yay");
+
+        final Meter meter = registry.meter(name(InstrumentedResourceMeteredPerClass.class, "meteredPerClass"));
+
+        assertThat(meter.getCount()).isEqualTo(1);
+    }
+
+    @Test
+    public void subresourcesFromLocatorsRegisterMetrics() {
+        assertThat(target("subresource/meteredPerClass")
+                .request()
+                .get(String.class))
+                .isEqualTo("yay");
+
+        final Meter meter = registry.meter(name(InstrumentedSubResourceMeteredPerClass.class, "meteredPerClass"));
+        assertThat(meter.getCount()).isEqualTo(1);
+
+    }
+
+
+}

metrics-jersey2/src/test/java/com/codahale/metrics/jersey2/SingletonMetricsTimedPerClassJerseyTest.java
@@ -1,0 +1,66 @@
+package com.codahale.metrics.jersey2;
+
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
+import com.codahale.metrics.jersey2.resources.InstrumentedResourceTimedPerClass;
+import com.codahale.metrics.jersey2.resources.InstrumentedSubResourceTimedPerClass;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import javax.ws.rs.core.Application;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static com.codahale.metrics.MetricRegistry.name;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests registering {@link InstrumentedResourceMethodApplicationListener} as a singleton
+ * in a Jersey {@link ResourceConfig}
+ */
+public class SingletonMetricsTimedPerClassJerseyTest extends JerseyTest {
+    static {
+        Logger.getLogger("org.glassfish.jersey").setLevel(Level.OFF);
+    }
+
+    private MetricRegistry registry;
+
+    @Override
+    protected Application configure() {
+        this.registry = new MetricRegistry();
+
+        ResourceConfig config = new ResourceConfig();
+
+        config = config.register(new MetricsFeature(this.registry));
+        config = config.register(InstrumentedResourceTimedPerClass.class);
+
+        return config;
+    }
+
+    @Test
+    public void timedPerClassMethodsAreTimed() {
+        assertThat(target("timedPerClass")
+                .request()
+                .get(String.class))
+                .isEqualTo("yay");
+
+        final Timer timer = registry.timer(name(InstrumentedResourceTimedPerClass.class, "timedPerClass"));
+
+        assertThat(timer.getCount()).isEqualTo(1);
+    }
+
+    @Test
+    public void subresourcesFromLocatorsRegisterMetrics() {
+        assertThat(target("subresource/timedPerClass")
+                .request()
+                .get(String.class))
+                .isEqualTo("yay");
+
+        final Timer timer = registry.timer(name(InstrumentedSubResourceTimedPerClass.class, "timedPerClass"));
+        assertThat(timer.getCount()).isEqualTo(1);
+
+    }
+
+
+}

metrics-jersey2/src/test/java/com/codahale/metrics/jersey2/resources/InstrumentedResourceExceptionMeteredPerClass.java
@@ -1,0 +1,28 @@
+package com.codahale.metrics.jersey2.resources;
+
+import com.codahale.metrics.annotation.ExceptionMetered;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+
+@ExceptionMetered(cause = IOException.class)
+@Path("/")
+@Produces(MediaType.TEXT_PLAIN)
+public class InstrumentedResourceExceptionMeteredPerClass {
+
+    @GET
+    @Path("/exception-metered")
+    public String exceptionMetered(@QueryParam("splode") @DefaultValue("false") boolean splode) throws IOException {
+        if (splode) {
+            throw new IOException("AUGH");
+        }
+        return "fuh";
+    }
+
+    @Path("/subresource")
+    public InstrumentedSubResourceExceptionMeteredPerClass locateSubResource() {
+        return new InstrumentedSubResourceExceptionMeteredPerClass();
+    }
+
+}

metrics-jersey2/src/test/java/com/codahale/metrics/jersey2/resources/InstrumentedResourceMeteredPerClass.java
@@ -1,0 +1,26 @@
+package com.codahale.metrics.jersey2.resources;
+
+import com.codahale.metrics.annotation.Metered;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Metered
+@Path("/")
+@Produces(MediaType.TEXT_PLAIN)
+public class InstrumentedResourceMeteredPerClass {
+
+    @GET
+    @Path("/meteredPerClass")
+    public String meteredPerClass() {
+        return "yay";
+    }
+
+    @Path("/subresource")
+    public InstrumentedSubResourceMeteredPerClass locateSubResource() {
+        return new InstrumentedSubResourceMeteredPerClass();
+    }
+
+}

metrics-jersey2/src/test/java/com/codahale/metrics/jersey2/resources/InstrumentedResourceTimedPerClass.java
@@ -1,0 +1,26 @@
+package com.codahale.metrics.jersey2.resources;
+
+import com.codahale.metrics.annotation.Timed;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Timed
+@Path("/")
+@Produces(MediaType.TEXT_PLAIN)
+public class InstrumentedResourceTimedPerClass {
+
+    @GET
+    @Path("/timedPerClass")
+    public String timedPerClass() {
+        return "yay";
+    }
+
+    @Path("/subresource")
+    public InstrumentedSubResourceTimedPerClass locateSubResource() {
+        return new InstrumentedSubResourceTimedPerClass();
+    }
+
+}

metrics-jersey2/src/test/java/com/codahale/metrics/jersey2/resources/InstrumentedSubResourceExceptionMeteredPerClass.java
@@ -1,0 +1,20 @@
+package com.codahale.metrics.jersey2.resources;
+
+import com.codahale.metrics.annotation.ExceptionMetered;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+
+@ExceptionMetered(cause = IOException.class)
+@Produces(MediaType.TEXT_PLAIN)
+public class InstrumentedSubResourceExceptionMeteredPerClass {
+    @GET
+    @Path("/exception-metered")
+    public String exceptionMetered(@QueryParam("splode") @DefaultValue("false") boolean splode) throws IOException {
+        if (splode) {
+            throw new IOException("AUGH");
+        }
+        return "fuh";
+    }
+}

metrics-jersey2/src/test/java/com/codahale/metrics/jersey2/resources/InstrumentedSubResourceMeteredPerClass.java
@@ -1,0 +1,18 @@
+package com.codahale.metrics.jersey2.resources;
+
+import com.codahale.metrics.annotation.Metered;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Metered
+@Produces(MediaType.TEXT_PLAIN)
+public class InstrumentedSubResourceMeteredPerClass {
+    @GET
+    @Path("/meteredPerClass")
+    public String meteredPerClass() {
+        return "yay";
+    }
+}