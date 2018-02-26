jboss-fuse/dropwizard-metrics

commit a01d5afabfae0071c5cb0c13738751e8d0eb42cb

Remove dead code from Slf4jReporter and its tests
Remove among the other things:

* Unnecessary reset of string builders
* Methods which accept only one argument

metrics-core/src/main/java/com/codahale/metrics/Slf4jReporter.java
@@ -219,7 +219,8 @@ public class Slf4jReporter extends ScheduledReporter {
                     loggerProxy = new DebugLoggerProxy(logger);
                     break;
             }
-            return new Slf4jReporter(registry, loggerProxy, marker, prefix, rateUnit, durationUnit, filter, executor, shutdownExecutorOnStop, disabledMetricAttributes);
+            return new Slf4jReporter(registry, loggerProxy, marker, prefix, rateUnit, durationUnit, filter, executor,
+                    shutdownExecutorOnStop, disabledMetricAttributes);
         }
     }
 
@@ -237,7 +238,8 @@ public class Slf4jReporter extends ScheduledReporter {
                           ScheduledExecutorService executor,
                           boolean shutdownExecutorOnStop,
                           Set<MetricAttribute> disabledMetricAttributes) {
-        super(registry, "logger-reporter", filter, rateUnit, durationUnit, executor, shutdownExecutorOnStop, disabledMetricAttributes);
+        super(registry, "logger-reporter", filter, rateUnit, durationUnit, executor, shutdownExecutorOnStop,
+                disabledMetricAttributes);
         this.loggerProxy = loggerProxy;
         this.marker = marker;
         this.prefix = prefix;
@@ -276,7 +278,6 @@ public class Slf4jReporter extends ScheduledReporter {
 
     private void logTimer(StringBuilder b, String name, Timer timer) {
         final Snapshot snapshot = timer.getSnapshot();
-        b.setLength(0);
         b.append("type=TIMER");
         append(b, "name", prefix(name));
         appendCountIfEnabled(b, timer);
@@ -297,7 +298,6 @@ public class Slf4jReporter extends ScheduledReporter {
     }
 
     private void logMeter(StringBuilder b, String name, Meter meter) {
-        b.setLength(0);
         b.append("type=METER");
         append(b, "name", prefix(name));
         appendCountIfEnabled(b, meter);
@@ -308,7 +308,6 @@ public class Slf4jReporter extends ScheduledReporter {
 
     private void logHistogram(StringBuilder b, String name, Histogram histogram) {
         final Snapshot snapshot = histogram.getSnapshot();
-        b.setLength(0);
         b.append("type=HISTOGRAM");
         append(b, "name", prefix(name));
         appendCountIfEnabled(b, histogram);
@@ -326,7 +325,6 @@ public class Slf4jReporter extends ScheduledReporter {
     }
 
     private void logCounter(StringBuilder b, String name, Counter counter) {
-        b.setLength(0);
         b.append("type=COUNTER");
         append(b, "name", prefix(name));
         append(b, COUNT.getCode(), counter.getCount());
@@ -334,32 +332,35 @@ public class Slf4jReporter extends ScheduledReporter {
     }
 
     private void logGauge(StringBuilder b, String name, Gauge<?> gauge) {
-        b.setLength(0);
         b.append("type=GAUGE");
         append(b, "name", prefix(name));
         append(b, "value", gauge.getValue());
         loggerProxy.log(marker, b.toString());
     }
 
-    private void appendLongDurationIfEnabled(StringBuilder b, MetricAttribute metricAttribute, Supplier<Long> durationSupplier) {
+    private void appendLongDurationIfEnabled(StringBuilder b, MetricAttribute metricAttribute,
+                                             Supplier<Long> durationSupplier) {
         if (!getDisabledMetricAttributes().contains(metricAttribute)) {
             append(b, metricAttribute.getCode(), convertDuration(durationSupplier.get()));
         }
     }
 
-    private void appendDoubleDurationIfEnabled(StringBuilder b, MetricAttribute metricAttribute, Supplier<Double> durationSupplier) {
+    private void appendDoubleDurationIfEnabled(StringBuilder b, MetricAttribute metricAttribute,
+                                               Supplier<Double> durationSupplier) {
         if (!getDisabledMetricAttributes().contains(metricAttribute)) {
             append(b, metricAttribute.getCode(), convertDuration(durationSupplier.get()));
         }
     }
 
-    private void appendLongIfEnabled(StringBuilder b, MetricAttribute metricAttribute, Supplier<Long> valueSupplier) {
+    private void appendLongIfEnabled(StringBuilder b, MetricAttribute metricAttribute,
+                                     Supplier<Long> valueSupplier) {
         if (!getDisabledMetricAttributes().contains(metricAttribute)) {
             append(b, metricAttribute.getCode(), valueSupplier.get());
         }
     }
 
-    private void appendDoubleIfEnabled(StringBuilder b, MetricAttribute metricAttribute, Supplier<Double> valueSupplier) {
+    private void appendDoubleIfEnabled(StringBuilder b, MetricAttribute metricAttribute,
+                                       Supplier<Double> valueSupplier) {
         if (!getDisabledMetricAttributes().contains(metricAttribute)) {
             append(b, metricAttribute.getCode(), valueSupplier.get());
         }

metrics-core/src/test/java/com/codahale/metrics/Slf4jReporterTest.java
@@ -64,16 +64,16 @@ public class Slf4jReporterTest {
 
     @Test
     public void reportsGaugeValuesAtErrorDefault() {
-        reportsGaugeValuesAtError("type=GAUGE, name=gauge, value=value");
+        reportsGaugeValuesAtError();
     }
 
     @Test
     public void reportsGaugeValuesAtErrorAllDisabled() {
         disabledMetricAttributes = EnumSet.allOf(MetricAttribute.class); // has no effect
-        reportsGaugeValuesAtError("type=GAUGE, name=gauge, value=value");
+        reportsGaugeValuesAtError();
     }
 
-    private void reportsGaugeValuesAtError(final String expectedLog) {
+    private void reportsGaugeValuesAtError() {
         when(logger.isErrorEnabled(marker)).thenReturn(true);
         errorReporter().report(map("gauge", () -> "value"),
                 map(),
@@ -81,22 +81,21 @@ public class Slf4jReporterTest {
                 map(),
                 map());
 
-        verify(logger).error(marker, expectedLog);
+        verify(logger).error(marker, "type=GAUGE, name=gauge, value=value");
     }
 
     @Test
     public void reportsCounterValuesAtErrorDefault() {
-        reportsCounterValuesAtError("type=COUNTER, name=test.counter, count=100");
+        reportsCounterValuesAtError();
     }
 
     @Test
     public void reportsCounterValuesAtErrorAllDisabled() {
         disabledMetricAttributes = EnumSet.allOf(MetricAttribute.class); // has no effect
-        reportsCounterValuesAtError("type=COUNTER, name=test.counter, count=100");
+        reportsCounterValuesAtError();
     }
 
-
-    private void reportsCounterValuesAtError(final String expectedLog) {
+    private void reportsCounterValuesAtError() {
         final Counter counter = mock(Counter.class);
         when(counter.getCount()).thenReturn(100L);
         when(logger.isErrorEnabled(marker)).thenReturn(true);
@@ -107,18 +106,20 @@ public class Slf4jReporterTest {
                 map(),
                 map());
 
-        verify(logger).error(marker, expectedLog);
+        verify(logger).error(marker, "type=COUNTER, name=test.counter, count=100");
     }
 
     @Test
     public void reportsHistogramValuesAtErrorDefault() {
-        reportsHistogramValuesAtError("type=HISTOGRAM, name=test.histogram, count=1, min=4, max=2, mean=3.0, stddev=5.0, p50=6.0, p75=7.0, p95=8.0, p98=9.0, p99=10.0, p999=11.0");
+        reportsHistogramValuesAtError("type=HISTOGRAM, name=test.histogram, count=1, min=4, " +
+                "max=2, mean=3.0, stddev=5.0, p50=6.0, p75=7.0, p95=8.0, p98=9.0, p99=10.0, p999=11.0");
     }
 
     @Test
     public void reportsHistogramValuesAtErrorWithDisabledMetricAttributes() {
         disabledMetricAttributes = EnumSet.of(COUNT, MIN, P50);
-        reportsHistogramValuesAtError("type=HISTOGRAM, name=test.histogram, max=2, mean=3.0, stddev=5.0, p75=7.0, p95=8.0, p98=9.0, p99=10.0, p999=11.0");
+        reportsHistogramValuesAtError("type=HISTOGRAM, name=test.histogram, max=2, mean=3.0, " +
+                "stddev=5.0, p75=7.0, p95=8.0, p98=9.0, p99=10.0, p999=11.0");
     }
 
     private void reportsHistogramValuesAtError(final String expectedLog) {
@@ -151,13 +152,15 @@ public class Slf4jReporterTest {
 
     @Test
     public void reportsMeterValuesAtErrorDefault() {
-        reportsMeterValuesAtError("type=METER, name=test.meter, count=1, m1_rate=3.0, m5_rate=4.0, m15_rate=5.0, mean_rate=2.0, rate_unit=events/second");
+        reportsMeterValuesAtError("type=METER, name=test.meter, count=1, m1_rate=3.0, m5_rate=4.0, " +
+                "m15_rate=5.0, mean_rate=2.0, rate_unit=events/second");
     }
 
     @Test
     public void reportsMeterValuesAtErrorWithDisabledMetricAttributes() {
         disabledMetricAttributes = EnumSet.of(MIN, P50, M1_RATE);
-        reportsMeterValuesAtError("type=METER, name=test.meter, count=1, m5_rate=4.0, m15_rate=5.0, mean_rate=2.0, rate_unit=events/second");
+        reportsMeterValuesAtError("type=METER, name=test.meter, count=1, m5_rate=4.0, m15_rate=5.0, " +
+                "mean_rate=2.0, rate_unit=events/second");
     }
 
     private void reportsMeterValuesAtError(final String expectedLog) {
@@ -180,13 +183,18 @@ public class Slf4jReporterTest {
 
     @Test
     public void reportsTimerValuesAtErrorDefault() {
-        reportsTimerValuesAtError("type=TIMER, name=test.another.timer, count=1, min=300.0, max=100.0, mean=200.0, stddev=400.0, p50=500.0, p75=600.0, p95=700.0, p98=800.0, p99=900.0, p999=1000.0, m1_rate=3.0, m5_rate=4.0, m15_rate=5.0, mean_rate=2.0, rate_unit=events/second, duration_unit=milliseconds");
+        reportsTimerValuesAtError("type=TIMER, name=test.another.timer, count=1, min=300.0, max=100.0, " +
+                "mean=200.0, stddev=400.0, p50=500.0, p75=600.0, p95=700.0, p98=800.0, p99=900.0, p999=1000.0, " +
+                "m1_rate=3.0, m5_rate=4.0, m15_rate=5.0, mean_rate=2.0, rate_unit=events/second, " +
+                "duration_unit=milliseconds");
     }
 
     @Test
     public void reportsTimerValuesAtErrorWithDisabledMetricAttributes() {
         disabledMetricAttributes = EnumSet.of(MIN, STDDEV, P999, MEAN_RATE);
-        reportsTimerValuesAtError("type=TIMER, name=test.another.timer, count=1, max=100.0, mean=200.0, p50=500.0, p75=600.0, p95=700.0, p98=800.0, p99=900.0, m1_rate=3.0, m5_rate=4.0, m15_rate=5.0, rate_unit=events/second, duration_unit=milliseconds");
+        reportsTimerValuesAtError("type=TIMER, name=test.another.timer, count=1, max=100.0, mean=200.0, " +
+                "p50=500.0, p75=600.0, p95=700.0, p98=800.0, p99=900.0, m1_rate=3.0, m5_rate=4.0, m15_rate=5.0, " +
+                "rate_unit=events/second, duration_unit=milliseconds");
     }
 
     private void reportsTimerValuesAtError(final String expectedLog) {
@@ -226,10 +234,6 @@ public class Slf4jReporterTest {
 
     @Test
     public void reportsGaugeValuesDefault() {
-        reportsGaugeValues("type=GAUGE, name=prefix.gauge, value=value");
-    }
-
-    private void reportsGaugeValues(final String expectedLog) {
         when(logger.isInfoEnabled(marker)).thenReturn(true);
         infoReporter().report(map("gauge", () -> "value"),
                 map(),
@@ -237,15 +241,12 @@ public class Slf4jReporterTest {
                 map(),
                 map());
 
-        verify(logger).info(marker, expectedLog);
+        verify(logger).info(marker, "type=GAUGE, name=prefix.gauge, value=value");
     }
+
 
     @Test
     public void reportsCounterValuesDefault() {
-        reportsCounterValues("type=COUNTER, name=prefix.test.counter, count=100");
-    }
-
-    private void reportsCounterValues(final String expectedLog) {
         final Counter counter = mock(Counter.class);
         when(counter.getCount()).thenReturn(100L);
         when(logger.isInfoEnabled(marker)).thenReturn(true);
@@ -256,15 +257,11 @@ public class Slf4jReporterTest {
                 map(),
                 map());
 
-        verify(logger).info(marker, expectedLog);
+        verify(logger).info(marker, "type=COUNTER, name=prefix.test.counter, count=100");
     }
 
     @Test
     public void reportsHistogramValuesDefault() {
-        reportsHistogramValues("type=HISTOGRAM, name=prefix.test.histogram, count=1, min=4, max=2, mean=3.0, stddev=5.0, p50=6.0, p75=7.0, p95=8.0, p98=9.0, p99=10.0, p999=11.0");
-    }
-
-    private void reportsHistogramValues(final String expectedLog) {
         final Histogram histogram = mock(Histogram.class);
         when(histogram.getCount()).thenReturn(1L);
 
@@ -289,15 +286,12 @@ public class Slf4jReporterTest {
                 map(),
                 map());
 
-        verify(logger).info(marker, expectedLog);
+        verify(logger).info(marker, "type=HISTOGRAM, name=prefix.test.histogram, count=1, min=4, max=2, mean=3.0, " +
+                "stddev=5.0, p50=6.0, p75=7.0, p95=8.0, p98=9.0, p99=10.0, p999=11.0");
     }
 
     @Test
     public void reportsMeterValuesDefault() {
-        reportsMeterValues("type=METER, name=prefix.test.meter, count=1, m1_rate=3.0, m5_rate=4.0, m15_rate=5.0, mean_rate=2.0, rate_unit=events/second");
-    }
-
-    private void reportsMeterValues(final String expectedLog) {
         final Meter meter = mock(Meter.class);
         when(meter.getCount()).thenReturn(1L);
         when(meter.getMeanRate()).thenReturn(2.0);
@@ -312,15 +306,12 @@ public class Slf4jReporterTest {
                 map("test.meter", meter),
                 map());
 
-        verify(logger).info(marker, expectedLog);
+        verify(logger).info(marker, "type=METER, name=prefix.test.meter, count=1, m1_rate=3.0, m5_rate=4.0, " +
+                "m15_rate=5.0, mean_rate=2.0, rate_unit=events/second");
     }
 
     @Test
     public void reportsTimerValuesDefault() {
-        reportsTimerValues("type=TIMER, name=prefix.test.another.timer, count=1, min=300.0, max=100.0, mean=200.0, stddev=400.0, p50=500.0, p75=600.0, p95=700.0, p98=800.0, p99=900.0, p999=1000.0, m1_rate=3.0, m5_rate=4.0, m15_rate=5.0, mean_rate=2.0, rate_unit=events/second, duration_unit=milliseconds");
-    }
-
-    public void reportsTimerValues(final String expectedLog) {
         final Timer timer = mock(Timer.class);
         when(timer.getCount()).thenReturn(1L);
 
@@ -351,7 +342,9 @@ public class Slf4jReporterTest {
                 map(),
                 map("test.another.timer", timer));
 
-        verify(logger).info(marker, expectedLog);
+        verify(logger).info(marker, "type=TIMER, name=prefix.test.another.timer, count=1, min=300.0, max=100.0, " +
+                "mean=200.0, stddev=400.0, p50=500.0, p75=600.0, p95=700.0, p98=800.0, p99=900.0, p999=1000.0," +
+                " m1_rate=3.0, m5_rate=4.0, m15_rate=5.0, mean_rate=2.0, rate_unit=events/second, duration_unit=milliseconds");
     }
 
     private <T> SortedMap<String, T> map() {

commit de1128ba27c30b4b14e167cdc76ea60303249c51

Correctly reuse the string builder across loggers of metrics
We have one string builder for every metric, hence we should reset
it before trying to build a new message in the text form.

The regression has been introduced in a01d5af.

metrics-core/src/main/java/com/codahale/metrics/Slf4jReporter.java
@@ -278,6 +278,7 @@ public class Slf4jReporter extends ScheduledReporter {
 
     private void logTimer(StringBuilder b, String name, Timer timer) {
         final Snapshot snapshot = timer.getSnapshot();
+        b.setLength(0);
         b.append("type=TIMER");
         append(b, "name", prefix(name));
         appendCountIfEnabled(b, timer);
@@ -298,6 +299,7 @@ public class Slf4jReporter extends ScheduledReporter {
     }
 
     private void logMeter(StringBuilder b, String name, Meter meter) {
+        b.setLength(0);
         b.append("type=METER");
         append(b, "name", prefix(name));
         appendCountIfEnabled(b, meter);
@@ -308,6 +310,7 @@ public class Slf4jReporter extends ScheduledReporter {
 
     private void logHistogram(StringBuilder b, String name, Histogram histogram) {
         final Snapshot snapshot = histogram.getSnapshot();
+        b.setLength(0);
         b.append("type=HISTOGRAM");
         append(b, "name", prefix(name));
         appendCountIfEnabled(b, histogram);
@@ -325,6 +328,7 @@ public class Slf4jReporter extends ScheduledReporter {
     }
 
     private void logCounter(StringBuilder b, String name, Counter counter) {
+        b.setLength(0);
         b.append("type=COUNTER");
         append(b, "name", prefix(name));
         append(b, COUNT.getCode(), counter.getCount());
@@ -332,6 +336,7 @@ public class Slf4jReporter extends ScheduledReporter {
     }
 
     private void logGauge(StringBuilder b, String name, Gauge<?> gauge) {
+        b.setLength(0);
         b.append("type=GAUGE");
         append(b, "name", prefix(name));
         append(b, "value", gauge.getValue());

metrics-core/src/test/java/com/codahale/metrics/Slf4jReporterTest.java
@@ -84,6 +84,69 @@ public class Slf4jReporterTest {
         verify(logger).error(marker, "type=GAUGE, name=gauge, value=value");
     }
 
+
+    private Timer timer() {
+        final Timer timer = mock(Timer.class);
+        when(timer.getCount()).thenReturn(1L);
+
+        when(timer.getMeanRate()).thenReturn(2.0);
+        when(timer.getOneMinuteRate()).thenReturn(3.0);
+        when(timer.getFiveMinuteRate()).thenReturn(4.0);
+        when(timer.getFifteenMinuteRate()).thenReturn(5.0);
+
+        final Snapshot snapshot = mock(Snapshot.class);
+        when(snapshot.getMax()).thenReturn(TimeUnit.MILLISECONDS.toNanos(100));
+        when(snapshot.getMean()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(200));
+        when(snapshot.getMin()).thenReturn(TimeUnit.MILLISECONDS.toNanos(300));
+        when(snapshot.getStdDev()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(400));
+        when(snapshot.getMedian()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(500));
+        when(snapshot.get75thPercentile()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(600));
+        when(snapshot.get95thPercentile()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(700));
+        when(snapshot.get98thPercentile()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(800));
+        when(snapshot.get99thPercentile()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(900));
+        when(snapshot.get999thPercentile()).thenReturn((double) TimeUnit.MILLISECONDS
+                .toNanos(1000));
+
+        when(timer.getSnapshot()).thenReturn(snapshot);
+        return timer;
+    }
+
+    private Histogram histogram() {
+        final Histogram histogram = mock(Histogram.class);
+        when(histogram.getCount()).thenReturn(1L);
+
+        final Snapshot snapshot = mock(Snapshot.class);
+        when(snapshot.getMax()).thenReturn(2L);
+        when(snapshot.getMean()).thenReturn(3.0);
+        when(snapshot.getMin()).thenReturn(4L);
+        when(snapshot.getStdDev()).thenReturn(5.0);
+        when(snapshot.getMedian()).thenReturn(6.0);
+        when(snapshot.get75thPercentile()).thenReturn(7.0);
+        when(snapshot.get95thPercentile()).thenReturn(8.0);
+        when(snapshot.get98thPercentile()).thenReturn(9.0);
+        when(snapshot.get99thPercentile()).thenReturn(10.0);
+        when(snapshot.get999thPercentile()).thenReturn(11.0);
+
+        when(histogram.getSnapshot()).thenReturn(snapshot);
+        return histogram;
+    }
+
+    private Meter meter() {
+        final Meter meter = mock(Meter.class);
+        when(meter.getCount()).thenReturn(1L);
+        when(meter.getMeanRate()).thenReturn(2.0);
+        when(meter.getOneMinuteRate()).thenReturn(3.0);
+        when(meter.getFiveMinuteRate()).thenReturn(4.0);
+        when(meter.getFifteenMinuteRate()).thenReturn(5.0);
+        return meter;
+    }
+
+    private Counter counter() {
+        final Counter counter = mock(Counter.class);
+        when(counter.getCount()).thenReturn(100L);
+        return counter;
+    }
+
     @Test
     public void reportsCounterValuesAtErrorDefault() {
         reportsCounterValuesAtError();
@@ -96,8 +159,7 @@ public class Slf4jReporterTest {
     }
 
     private void reportsCounterValuesAtError() {
-        final Counter counter = mock(Counter.class);
-        when(counter.getCount()).thenReturn(100L);
+        final Counter counter = counter();
         when(logger.isErrorEnabled(marker)).thenReturn(true);
 
         errorReporter().report(map(),
@@ -123,22 +185,7 @@ public class Slf4jReporterTest {
     }
 
     private void reportsHistogramValuesAtError(final String expectedLog) {
-        final Histogram histogram = mock(Histogram.class);
-        when(histogram.getCount()).thenReturn(1L);
-
-        final Snapshot snapshot = mock(Snapshot.class);
-        when(snapshot.getMax()).thenReturn(2L);
-        when(snapshot.getMean()).thenReturn(3.0);
-        when(snapshot.getMin()).thenReturn(4L);
-        when(snapshot.getStdDev()).thenReturn(5.0);
-        when(snapshot.getMedian()).thenReturn(6.0);
-        when(snapshot.get75thPercentile()).thenReturn(7.0);
-        when(snapshot.get95thPercentile()).thenReturn(8.0);
-        when(snapshot.get98thPercentile()).thenReturn(9.0);
-        when(snapshot.get99thPercentile()).thenReturn(10.0);
-        when(snapshot.get999thPercentile()).thenReturn(11.0);
-
-        when(histogram.getSnapshot()).thenReturn(snapshot);
+        final Histogram histogram = histogram();
         when(logger.isErrorEnabled(marker)).thenReturn(true);
 
         errorReporter().report(map(),
@@ -164,12 +211,7 @@ public class Slf4jReporterTest {
     }
 
     private void reportsMeterValuesAtError(final String expectedLog) {
-        final Meter meter = mock(Meter.class);
-        when(meter.getCount()).thenReturn(1L);
-        when(meter.getMeanRate()).thenReturn(2.0);
-        when(meter.getOneMinuteRate()).thenReturn(3.0);
-        when(meter.getFiveMinuteRate()).thenReturn(4.0);
-        when(meter.getFifteenMinuteRate()).thenReturn(5.0);
+        final Meter meter = meter();
         when(logger.isErrorEnabled(marker)).thenReturn(true);
 
         errorReporter().report(map(),
@@ -180,6 +222,7 @@ public class Slf4jReporterTest {
 
         verify(logger).error(marker, expectedLog);
     }
+
 
     @Test
     public void reportsTimerValuesAtErrorDefault() {
@@ -198,28 +241,7 @@ public class Slf4jReporterTest {
     }
 
     private void reportsTimerValuesAtError(final String expectedLog) {
-        final Timer timer = mock(Timer.class);
-        when(timer.getCount()).thenReturn(1L);
-
-        when(timer.getMeanRate()).thenReturn(2.0);
-        when(timer.getOneMinuteRate()).thenReturn(3.0);
-        when(timer.getFiveMinuteRate()).thenReturn(4.0);
-        when(timer.getFifteenMinuteRate()).thenReturn(5.0);
-
-        final Snapshot snapshot = mock(Snapshot.class);
-        when(snapshot.getMax()).thenReturn(TimeUnit.MILLISECONDS.toNanos(100));
-        when(snapshot.getMean()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(200));
-        when(snapshot.getMin()).thenReturn(TimeUnit.MILLISECONDS.toNanos(300));
-        when(snapshot.getStdDev()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(400));
-        when(snapshot.getMedian()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(500));
-        when(snapshot.get75thPercentile()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(600));
-        when(snapshot.get95thPercentile()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(700));
-        when(snapshot.get98thPercentile()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(800));
-        when(snapshot.get99thPercentile()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(900));
-        when(snapshot.get999thPercentile()).thenReturn((double) TimeUnit.MILLISECONDS
-                .toNanos(1000));
-
-        when(timer.getSnapshot()).thenReturn(snapshot);
+        final Timer timer = timer();
 
         when(logger.isErrorEnabled(marker)).thenReturn(true);
 
@@ -247,8 +269,7 @@ public class Slf4jReporterTest {
 
     @Test
     public void reportsCounterValuesDefault() {
-        final Counter counter = mock(Counter.class);
-        when(counter.getCount()).thenReturn(100L);
+        final Counter counter = counter();
         when(logger.isInfoEnabled(marker)).thenReturn(true);
 
         infoReporter().report(map(),
@@ -262,22 +283,7 @@ public class Slf4jReporterTest {
 
     @Test
     public void reportsHistogramValuesDefault() {
-        final Histogram histogram = mock(Histogram.class);
-        when(histogram.getCount()).thenReturn(1L);
-
-        final Snapshot snapshot = mock(Snapshot.class);
-        when(snapshot.getMax()).thenReturn(2L);
-        when(snapshot.getMean()).thenReturn(3.0);
-        when(snapshot.getMin()).thenReturn(4L);
-        when(snapshot.getStdDev()).thenReturn(5.0);
-        when(snapshot.getMedian()).thenReturn(6.0);
-        when(snapshot.get75thPercentile()).thenReturn(7.0);
-        when(snapshot.get95thPercentile()).thenReturn(8.0);
-        when(snapshot.get98thPercentile()).thenReturn(9.0);
-        when(snapshot.get99thPercentile()).thenReturn(10.0);
-        when(snapshot.get999thPercentile()).thenReturn(11.0);
-
-        when(histogram.getSnapshot()).thenReturn(snapshot);
+        final Histogram histogram = histogram();
         when(logger.isInfoEnabled(marker)).thenReturn(true);
 
         infoReporter().report(map(),
@@ -292,12 +298,7 @@ public class Slf4jReporterTest {
 
     @Test
     public void reportsMeterValuesDefault() {
-        final Meter meter = mock(Meter.class);
-        when(meter.getCount()).thenReturn(1L);
-        when(meter.getMeanRate()).thenReturn(2.0);
-        when(meter.getOneMinuteRate()).thenReturn(3.0);
-        when(meter.getFiveMinuteRate()).thenReturn(4.0);
-        when(meter.getFifteenMinuteRate()).thenReturn(5.0);
+        final Meter meter = meter();
         when(logger.isInfoEnabled(marker)).thenReturn(true);
 
         infoReporter().report(map(),
@@ -312,28 +313,7 @@ public class Slf4jReporterTest {
 
     @Test
     public void reportsTimerValuesDefault() {
-        final Timer timer = mock(Timer.class);
-        when(timer.getCount()).thenReturn(1L);
-
-        when(timer.getMeanRate()).thenReturn(2.0);
-        when(timer.getOneMinuteRate()).thenReturn(3.0);
-        when(timer.getFiveMinuteRate()).thenReturn(4.0);
-        when(timer.getFifteenMinuteRate()).thenReturn(5.0);
-
-        final Snapshot snapshot = mock(Snapshot.class);
-        when(snapshot.getMax()).thenReturn(TimeUnit.MILLISECONDS.toNanos(100));
-        when(snapshot.getMean()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(200));
-        when(snapshot.getMin()).thenReturn(TimeUnit.MILLISECONDS.toNanos(300));
-        when(snapshot.getStdDev()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(400));
-        when(snapshot.getMedian()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(500));
-        when(snapshot.get75thPercentile()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(600));
-        when(snapshot.get95thPercentile()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(700));
-        when(snapshot.get98thPercentile()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(800));
-        when(snapshot.get99thPercentile()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(900));
-        when(snapshot.get999thPercentile()).thenReturn((double) TimeUnit.MILLISECONDS
-                .toNanos(1000));
-
-        when(timer.getSnapshot()).thenReturn(snapshot);
+        final Timer timer = timer();
         when(logger.isInfoEnabled(marker)).thenReturn(true);
 
         infoReporter().report(map(),
@@ -343,6 +323,28 @@ public class Slf4jReporterTest {
                 map("test.another.timer", timer));
 
         verify(logger).info(marker, "type=TIMER, name=prefix.test.another.timer, count=1, min=300.0, max=100.0, " +
+                "mean=200.0, stddev=400.0, p50=500.0, p75=600.0, p95=700.0, p98=800.0, p99=900.0, p999=1000.0," +
+                " m1_rate=3.0, m5_rate=4.0, m15_rate=5.0, mean_rate=2.0, rate_unit=events/second, duration_unit=milliseconds");
+    }
+
+
+    @Test
+    public void reportsAllMetricsDefault() {
+        when(logger.isInfoEnabled(marker)).thenReturn(true);
+
+        infoReporter().report(map("test.gauge", () -> "value"),
+                map("test.counter", counter()),
+                map("test.histogram", histogram()),
+                map("test.meter", meter()),
+                map("test.timer", timer()));
+
+        verify(logger).info(marker, "type=GAUGE, name=prefix.test.gauge, value=value");
+        verify(logger).info(marker, "type=COUNTER, name=prefix.test.counter, count=100");
+        verify(logger).info(marker, "type=HISTOGRAM, name=prefix.test.histogram, count=1, min=4, max=2, mean=3.0, " +
+                "stddev=5.0, p50=6.0, p75=7.0, p95=8.0, p98=9.0, p99=10.0, p999=11.0");
+        verify(logger).info(marker, "type=METER, name=prefix.test.meter, count=1, m1_rate=3.0, m5_rate=4.0, " +
+                "m15_rate=5.0, mean_rate=2.0, rate_unit=events/second");
+        verify(logger).info(marker, "type=TIMER, name=prefix.test.timer, count=1, min=300.0, max=100.0, " +
                 "mean=200.0, stddev=400.0, p50=500.0, p75=600.0, p95=700.0, p98=800.0, p99=900.0, p999=1000.0," +
                 " m1_rate=3.0, m5_rate=4.0, m15_rate=5.0, mean_rate=2.0, rate_unit=events/second, duration_unit=milliseconds");
     }